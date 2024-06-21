1f33c3fd534fdb93e8438140124cf4e9bbc54aa4
Carol García <jake.v.garcia.mazatlan@gmail.com>
2024-06-21 09:12:39 -0700

    Se corrigieron errores en el codigo

--- app/src/main/java/com/example/appmicalculadora/MainActivity.kt
+++ app/src/main/java/com/example/appmicalculadora/MainActivity.kt
@@ -12,63 +12,42 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var txtUsuario : EditText;
-    private lateinit var txtConstraseña : EditText;
-    private lateinit var btnIngresar : Button;
-    private lateinit var btnSalir : Button;
-
+    private lateinit var txtUsuario: EditText
+    private lateinit var txtContraseña: EditText
+    private lateinit var btbIngresar: Button
+    private lateinit var btnSalir: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        //INICIAR COMPONENTES
-        iniciarComponentes()
-        eventosClic()
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        iniciarComponetes()
+        eventosClick()
     }
 
-    public fun iniciarComponentes(){
-        txtUsuario = findViewById(R.id.txtUsuario)
-        txtConstraseña = findViewById(R.id.txtContraseña)
-        btnIngresar = findViewById(R.id.btnIngresar)
-        btnSalir = findViewById(R.id.btnSalir)
+    public fun iniciarComponetes() {
+        txtUsuario = findViewById(R.id.txtUsuario) as EditText
+        txtContraseña = findViewById(R.id.txtContraseña) as EditText
+        btnSalir = findViewById(R.id.btnSalir) as Button
+        btbIngresar = findViewById(R.id.btnIngresar) as Button
     }
 
-    public fun eventosClic(){
-        btnIngresar.setOnClickListener(View.OnClickListener {
-
-            var usuario : String = getString(R.string.usuario)
-            var pas : String = getString(R.string.pass)
-            var nombre : String = getString(R.string.nombre)
-
-            if (txtUsuario.text.toString().contentEquals(usuario) && txtConstraseña.text.toString().contentEquals(pas)){
-
-                var intet = Intent(this, OperacionesActivity::class.java)
-                    intet.putExtra("Nombre", nombre)
-                startActivity(intet)
-
-            }
-            else
-
-                Toast.makeText(this, "El usuario o password es incorrecto", Toast.LENGTH_SHORT).show()
-
+    public fun eventosClick() {
+        btbIngresar.setOnClickListener(View.OnClickListener {
+            var usuario: String = getString(R.string.usuario)
+            var pass: String = getString(R.string.pass)
+            if (txtUsuario.text.toString().contentEquals(usuario) &&
+                txtContraseña.text.toString().contentEquals(pass)
+            ) {
+                val intent = Intent(this, OperacionesActivity::class.java)
+                intent.putExtra("Usuario", txtUsuario.text.toString())
+                startActivity(intent)
+            } else
+                Toast.makeText(
+                    this, "Usuario o contraseña no validos",
+                    Toast.LENGTH_SHORT
+                ).show()
         })
-
         btnSalir.setOnClickListener(View.OnClickListener {
-
             finish()
         })
-
-
     }
-
-
-
 }
--- app/src/main/java/com/example/appmicalculadora/OperacionesActivity.kt
+++ app/src/main/java/com/example/appmicalculadora/OperacionesActivity.kt
@@ -1,68 +1,107 @@
 package com.example.appmicalculadora
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class OperacionesActivity : AppCompatActivity() {
-
     private lateinit var txtUsuario : TextView
     private lateinit var txtNum1 : EditText
     private lateinit var txtNum2 : EditText
     private lateinit var txtResultado : TextView
-
     private lateinit var btnSumar : Button
     private lateinit var btnRestar : Button
-    private lateinit var btnMultiplicar : Button
+    private lateinit var btnMult : Button
     private lateinit var btnDiv : Button
-
+    private lateinit var btnCerrar : Button
     private lateinit var btnLimpiar : Button
-    private lateinit var btnRegresar : Button
-
-
     private lateinit var operaciones: Operaciones
-
+    var opcion : Int =0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_operaciones)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        iniciarCompontes()
+        eventosClic()
     }
-
-
-    public fun iniciarComponentes(){
-        txtUsuario = findViewById(R.id.txtUsuario)
+    public fun iniciarCompontes(){
+        txtUsuario = findViewById(R.id.txtNombre)
         txtResultado = findViewById(R.id.txtResultado)
         txtNum1 = findViewById(R.id.txtNum1)
         txtNum2 = findViewById(R.id.txtNum2)
-
-        btnSumar = findViewById(R.id.btnSumar)
-        btnRestar = findViewById(R.id.btnRestar)
-        btnMultiplicar = findViewById(R.id.btnMultiplicar)
+        btnSumar = findViewById(R.id.btnMas)
+        btnRestar = findViewById(R.id.btnRes)
+        btnMult = findViewById(R.id.btnMul)
         btnDiv = findViewById(R.id.btnDiv)
-
-        btnRegresar = findViewById(R.id.btnRegresar)
+        btnCerrar = findViewById(R.id.btnCerrar)
         btnLimpiar = findViewById(R.id.btnLimpiar)
-
-        val bundle : Bundle? = intent.extras
-        txtUsuario.text = bundle?.getString("nombre")
+        val bundle: Bundle? = intent.extras
+        txtUsuario.text = bundle?.getString("Usuario")
     }
-
-    public fun validar() : Boolean{
-
-        if (txtNum1.text.toString().contentEquals(" ") || txtNum2.text.toString().contentEquals(""))return false
-
+    public fun validar() : Boolean {
+        if( txtNum1.text.toString().contentEquals("") ||
+            txtNum2.text.toString().contentEquals("")) return false
         else return true
     }
-
+    public fun operacion(): Float{
+        var num1 : Float = 0.0f
+        var num2 : Float =0.0f
+        var res : Float =0.0f
+        if(validar()){
+            num1 = txtNum1.text.toString().toFloat()
+            num2 = txtNum2.text.toString().toFloat()
+            operaciones = Operaciones(num1,num2)
+            when(opcion){
+                1 -> {res=operaciones.suma()}
+                2 -> {res= operaciones.resta()}
+                3 -> {res= operaciones.multiplicacion()}
+                4 -> {res= operaciones.division()}
+            }
+        } else Toast.makeText(this,"Falto capturar informacion",Toast.LENGTH_SHORT).show()
+            return res;
+    }
+    public fun eventosClic(){
+        btnSumar.setOnClickListener(View.OnClickListener {
+            opcion =1;
+            txtResultado.text= operacion().toString()
+        })
+        btnRestar.setOnClickListener(View.OnClickListener {
+            opcion =2
+            txtResultado.text= operacion().toString()
+        })
+        btnMult.setOnClickListener(View.OnClickListener {
+            opcion =3
+            txtResultado.text= operacion().toString()
+        })
+        btnDiv.setOnClickListener(View.OnClickListener {
+            if(this.txtNum2.text.toString().toFloat()==0f)
+                txtResultado.text="No es posible dividir sobre 0"
+            else {
+                opcion = 4
+                txtResultado.text= operacion().toString()
+            }
+        })
+        btnLimpiar.setOnClickListener(View.OnClickListener {
+            txtResultado.text =""
+            txtNum2.text.clear()
+            txtNum1.text.clear()
+        })
+        btnCerrar.setOnClickListener(View.OnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Calculadora")
+            builder.setMessage("¿ Desea Cerrar ?")
+            builder.setPositiveButton(android.R.string.yes) { dialog,
+                                                              which ->
+                this.finish()
+            }
+            builder.setNegativeButton(android.R.string.no) { dialog, which ->
+            }
+            builder.show()
+        })
+    }
 }
